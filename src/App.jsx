0c695be955058fe5c2f215312d75aa35c8ccc701
Junaid Parkar <100304984+JunaidParkar@users.noreply.github.com>
2023-08-02 21:07:19 +0530

    Created perfectly orking chat app

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Chat from './components/Chat';
+
+import { auth } from './firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+
+import "./index.css"
+
+const style = {
+  appContainer: `max-w-[728px] mx-auto text-center`,
+  sectionContainer: `flex flex-col bg-gray-100 mt-10 shadow-xl border relative`,
+};
+
+function App() {
+  const [user] = useAuthState(auth);
+  return (
+    <div className={style.appContainer}>
+      <section className={style.sectionContainer}>
+        <Navbar />
+        {user ? <Chat /> : null}
+      </section>
+    </div>
+  );
+}
+
+export default App;
